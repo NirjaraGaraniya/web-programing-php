e48dc807f531c5f75e556ff9bc5c4cf60be55a02
NirjaraGaraniya <nirjaramjk2021@gmail.com>
2024-09-13 12:42:55 +0530

    Add files via upload

---
+++ Task1/script6.php
@@ -0,0 +1,12 @@
+<?php
+ echo "<b>Welcome to MCA Department</b><br><br>";
+echo "<i>Im Garaniya Nirjara </i><br>";
+echo "<h1>Hello Good Morning</h1><br>
+	<h2>Hello Good Morning</h2><br>
+	<h3>Hello Good Morning</h3><br>
+	<h4>Hello Good Morning</h4><br>
+	<h5>Hello Good Morning</h5><br>
+	<h6>Hello Good Morning</h6><br>";
+	
+
+?>
