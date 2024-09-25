5d3922c43f90c70063a6bc690bcdfc867b9fd257
NirjaraGaraniya <nirjaramjk2021@gmail.com>
2024-09-25 08:20:38 +0530

    Delete Script10.php

--- Script10.php
+++
@@ -1,9 +0,0 @@
-<?php
-function rect_area($length = 2, $width = 4) 
-{
-    $area = $length * $width;
-    echo "Area Of Rectangle with length " . $length . " & width " . $width . " is " . $area ;
-}
-rect_area(); 
-
-?>
